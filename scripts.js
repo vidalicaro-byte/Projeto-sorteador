f8f54a4e9c712d58ec086e753b0b2c3bb8912507
vidalicaro-byte <vidalicaro@hotmail.com>
2026-03-18 21:44:58 -0300

    JS resposta no paragrafo

--- scripts.js
+++ scripts.js
@@ -12,13 +12,13 @@ button.addEventListener("click", function () {
     const minInput = Math.ceil(Number(document.querySelector(".input1").value));
     const maxInput = Math.floor(Number(document.querySelector(".input2").value));
     const randomNumber = Math.floor(Math.random() * (maxInput - minInput + 1)) + minInput;
-
+    let resposta = document.querySelector(".paragrafo-resposta");
    if (minInput > maxInput) {
         alert("O primeiro número deve ser menor que o segundo número. Por favor, insira os valores corretamente.");
 
      }
 
-     else  alert(`O número sorteado é: ${randomNumber}`);
+     else  resposta.innerHTML= (`O número sorteado é: ${randomNumber}`);
 
 }
 )
